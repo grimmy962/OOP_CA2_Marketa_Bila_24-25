aea74f25a60dd9331d211747ae4602dee3dc690c
grimmy962 <145358834+grimmy962@users.noreply.github.com>
2024-11-29 23:17:42 +0000

    Q3 done
    - tested and everything works
    - there was just a problem with the closing bracket part of the code, but it should be alright this way
    - maybe ask if the txt files should be displayed for the user, since it's just printing the file names and if it's valid or not

--- src/main/java/org/example/Question3.java
+++ src/main/java/org/example/Question3.java
@@ -1,12 +1,16 @@
 package org.example;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
- *  Name:
- *  Class Group:
+ *  Name: Marketa Bila
+ *  Class Group: GD2A
  */
+
+//TODO: After being done with the questions, add the though process that's on the paper
 public class Question3  {   //Nested HTML (Stack)
 
     /*
@@ -14,7 +18,26 @@ filename: name of the file to test.
 */
     public static boolean validate(String filename) throws FileNotFoundException
     {
-        return false;
+        Stack<String> tagStack = new Stack<>();
+        File file = new File(filename);
+        Scanner keyboard = new Scanner(file);
+
+        //read all those tags, store them and then check for the closing tag
+        while(keyboard.hasNext()){
+            String tag = keyboard.next();
+            if(tag.startsWith("</")){
+                if(tagStack.isEmpty() || !tagStack.pop().equals(tag.substring(2, tag.length() - 1))){
+                    keyboard.close();
+                    return false;
+                }
+            }
+            //
+            else if(tag.startsWith("<") && tag.endsWith(">") && !tag.equals("<br>")){
+                tagStack.push(tag.substring(1, tag.length()-1));
+            }
+        }
+        keyboard.close();
+        return tagStack.isEmpty();
     }
 
     /*
