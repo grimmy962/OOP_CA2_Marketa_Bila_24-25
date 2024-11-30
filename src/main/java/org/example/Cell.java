3ddcba8aadc35754b5b2727d5210d30f9211c748
grimmy962 <145358834+grimmy962@users.noreply.github.com>
2024-11-30 00:51:11 +0000

    Q4 done
    - question done
    - make sure that everything works again tomorrow, do some testing
    - have to add notes as well
    - check if there is no error with the direction code and just the floodFill method in general

---
+++ src/main/java/org/example/Cell.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Cell {
+    int col, row;
+
+    public Cell(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+}
--- src/main/java/org/example/Question4.java
+++ src/main/java/org/example/Question4.java
@@ -2,10 +2,11 @@ package org.example;
 
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
- *  Name:
- *  Class Group:
+ *  Name: Marketa Bila
+ *  Class Group: GD2A
  */
 
 public class Question4  // Flood Fill (Stack, 2D Array)
@@ -15,12 +16,29 @@ public class Question4  // Flood Fill (Stack, 2D Array)
     }
     public static void start()
     {
+        //set up valunbles
         int[][] arr = floodFillStart();
+        Scanner keyboard = new Scanner(System.in);
+        int rowStart = 0;
+        int colStart = 0;
+
+        //ask the user for an input
+        System.out.println("Please enter the starting row: 0 - 9");
+        rowStart= keyboard.nextInt();
+
+        System.out.println("Please enter the starting column: 0 - 9");
+        colStart = keyboard.nextInt();
+
+        floodFill(rowStart, colStart, arr);
+        System.out.println("The final filled 2d array:");
+        display(arr);
     }
 
     /*
         Starter function to create the 2D array and populate it with zeros
      */
+
+    //make the 2d array, put everything at 0 to change it later
     public static int[][]  floodFillStart() {
         Scanner kb = new Scanner(System.in);
         int[][] arr = new int[10][10];
@@ -47,9 +65,29 @@ public class Question4  // Flood Fill (Stack, 2D Array)
             System.out.println();
         }
     }
-    private static void fill(int r, int c, int[][] arr)
+    private static void floodFill(int r, int c, int[][] arr)
     {
+        Stack<Cell> stack = new Stack<>();
+        stack.push(new Cell(r,c));
+        int fillValue = 1;
 
+        while(!stack.isEmpty()){
+            Cell current = stack.pop();
+
+            //check if the the cell is filled or not
+            if(current.row < 0 || current.row >=10 || current.col < 0 || current.col >=10 || arr[current.row][current.col] !=0){
+                continue;
+            }
+
+            //something to fill the cell and add a number to it
+            arr[current.row][current.col] = fillValue++;
+
+            //add movement based on the location, t
+            stack.push(new Cell(current.row -1, current.col));
+            stack.push(new Cell(current.row +1, current.col));
+            stack.push(new Cell(current.row, current.col -1));
+            stack.push(new Cell(current.row, current.col +1));
+        }
     }
 
 }
