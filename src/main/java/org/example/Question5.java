19a6f399c9b73ef914f56326b32213bb56183a04
grimmy962 <145358834+grimmy962@users.noreply.github.com>
2024-12-05 10:23:24 +0000

    Q5 done
    - it's done but feels messy
    - try to think of another way to separate the trailing spaces from words
    - try to make the displayed results more readable

--- src/main/java/org/example/Question5.java
+++ src/main/java/org/example/Question5.java
@@ -1,19 +1,53 @@
 package org.example;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.*;
 
 /**
- *  Name:
- *  Class Group:
+ *  Name: Marketa Bila
+ *  Class Group: GD2A
  */
 public class Question5 {    //Java Identifier Count (Map)
 
-    public static void readFile(String fileName)
-    {
+    public static void readFile(String fileName) throws FileNotFoundException {
+        Map<String, Integer> identifierCountMap = new HashMap<>();
+        Map<String, List<String>> identifierLines = new HashMap<>();
+        int lineNum =1;
+        Scanner keyboard = new Scanner(new File(fileName));
 
+        while(keyboard.hasNextLine()){
+            String line = keyboard.nextLine().trim();
+            String[] identifiers = line.split("[^A-Za-z0-9_]+");
+
+            for(String identifier : identifiers){
+                if(!identifier.isEmpty()){
+                    identifierCountMap.put(identifier, identifierCountMap.getOrDefault(identifier, 0)+1);
+                    identifierLines.putIfAbsent(identifier, new ArrayList<>());
+                    identifierLines.get(identifier).add(lineNum + ". " + line);
+                }
+            }
+
+            lineNum++;
+        }
+        keyboard.close();
+        displayResults(identifierCountMap, identifierLines);
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        readFile("src/Question2.java");
+        readFile("fileName.txt");
+    }
+
+    private static void displayResults (Map<String, Integer> identifierCountMap, Map<String, List<String>> identifierLines) {
+        List<String> sortedIdentifiers = new ArrayList<>(identifierCountMap.keySet());
+        Collections.sort(sortedIdentifiers);
+
+        for(String identifier : sortedIdentifiers){
+            System.out.println(identifier + " " + identifierCountMap.get(identifier));
+
+            for(String line : identifierLines.get(identifier)) {
+                System.out.println(" " +line);
+            }
+        }
     }
 }
